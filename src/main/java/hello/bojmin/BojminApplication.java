e848916e0d18b53344aa47369b2ed8829affc751
mimddon <james990828@naver.com>
2023-09-20 14:19:06 +0900

    initial commit

---
+++ src/main/java/hello/bojmin/BojminApplication.java
@@ -0,0 +1,13 @@
+package hello.bojmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BojminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BojminApplication.class, args);
+    }
+
+}
