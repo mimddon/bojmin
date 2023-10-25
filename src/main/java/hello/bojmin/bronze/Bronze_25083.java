a9f234ee4e2568b43b3a9d4589aa95a5e61b0bff
mimddon <james990828@naver.com>
2023-10-25 18:19:51 +0900

    bronze 25083

---
+++ src/main/java/hello/bojmin/bronze/Bronze_25083.java
@@ -0,0 +1,7 @@
+package hello.bojmin.bronze;
+
+public class Bronze_25083 {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7" + "\n" + "r`-_   ,'  ,/" + "\n" + " \\. \". L_r'" + "\n" + "   `~\\/" + "\n" + "      |" + "\n" + "      |");
+    }
+}
