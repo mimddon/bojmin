3026ca39e9a1c09ec86b8dfdd3403de2b74da3fb
mimddon <james990828@naver.com>
2023-11-08 01:55:39 +0900

    bronze 2869

---
+++ src/main/java/hello/bojmin/bronze/Bronze_2869.java
@@ -0,0 +1,23 @@
+package hello.bojmin.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze_2869 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int days = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            days++;
+        }
+
+        System.out.println(days);
+    }
+}
