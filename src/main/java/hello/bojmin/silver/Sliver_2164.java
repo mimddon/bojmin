4fba2a0a0ab210c51782055a1acc801d64f18717
mimddon <james990828@naver.com>
2023-11-09 23:29:04 +0900

    sliver 2164

---
+++ src/main/java/hello/bojmin/silver/Sliver_2164.java
@@ -0,0 +1,31 @@
+package hello.bojmin.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Sliver_2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            if (queue.size() <= 1) {
+                break;
+            }
+            int back = queue.poll();
+
+            queue.add(back);
+        }
+        System.out.println(queue.poll());
+    }
+
+}
